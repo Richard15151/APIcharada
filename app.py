e7fd570195b1fa0a633ce2d9c3473941c6208640
richard <richard.oliveira.senai@gmail.com>
2025-04-24 19:26:00 -0300

    tirandoapprun

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def charada_lista():
     else:
         return jsonify({'mensagem':'ERRO, nenhuma charada encontrada'}), 404
     
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
